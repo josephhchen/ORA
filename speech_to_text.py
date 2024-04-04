1e5fec115cdae532fd2218e4d4fba63141780e3b
Joseph Chen <joehaiyuechen@gmail.com>
2024-04-04 13:13:35 -0400

    Speech to Text finished, time to work on Model

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,63 +1,44 @@
 import queue
 import re
 import sys
-
 from google.cloud import speech
-
 import pyaudio
 
 RATE = 16000
 CHUNK = int(RATE / 10)
 
 class MicrophoneStream:
-
-    def __init__(self: object, rate: int = RATE, chunk: int = CHUNK) -> None:
+    def __init__(self, rate=RATE, chunk=CHUNK):
         self._rate = rate
         self._chunk = chunk
-
         self._buff = queue.Queue()
         self.closed = True
-    
-    def __enter__(self.object) -> object:
-        self._audio_interface = pyaudio.PyAudio()
-        self._audio_stream - self._audio_interface.open(
-            format=pyaudio.paInt16
 
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(  
+            format=pyaudio.paInt16,
             channels=1,
             rate=self._rate,
             input=True,
             frames_per_buffer=self._chunk,
             stream_callback=self._fill_buffer,
         )
-
         self.closed = False
-
         return self
 
-    def __exit__(
-            self: object,
-            type: object,
-            value: object,
-            traceback: object
-    ) -> None:
-        self.audio_stream.stop_stream()
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
         self._audio_stream.close()
         self.closed = True
         self._buff.put(None)
         self._audio_interface.terminate()
 
-    def _fill_buffer(
-        self: object,
-        in_data: object,
-        frame_count: int,
-        time_info: object,
-        status_flags: object,
-    ) -> object:
-        
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
         self._buff.put(in_data)
         return None, pyaudio.paContinue
 
-    def generator(self: object) -> object:
+    def generator(self):
         while not self.closed:
             chunk = self._buff.get()
             if chunk is None:
@@ -73,61 +54,54 @@ class MicrophoneStream:
                 except queue.Empty:
                     break
             yield b"".join(data)
-    def listen_print_loop(responses: object) -> str:
 
-        num_chars_printed = 0
-        for response in responses:
-            if not response.results:
-                continue
-            
-            result = response.results[0]
-            if not result.alternatives:
-                continue
-            
-            transcript = result.alternatives[0].transcript
-            overwrite_chars = " " * (num_chars_printed - len(transcript))
-
-            if not result.is_final:
-                sys.stdout.write(transcript + overwrite_chars + "\r")
-                sys.stdout.flush()
-
-                num_chars_printed = len(transcript)
-            else:
-                print(transcript + overwrite_chars)
-
-                if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                    print("Exiting...")
-                    break
-                num_chars_printed = 0
-
-        return transcript    
-    
-    def main() -> None:
-
-        language_code = "en-US"
-
-        client = speech.SpeechClient()
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz = RATE,
-            language_code=language_code,
-        )
+def listen_print_loop(responses):
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
         
-        streaming_config = speech.StreamingRecognitionConfig(
-            config=config,
-            interim_results = True
-        )
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+        
+        transcript = result.alternatives[0].transcript
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
 
-        with MicrophoneStream(RATE, CHUNK) as stream:
-            audio_generator = stream.generator()
-            requests = (
-                speech.StreamingRecognizeRequest(audio_content = content)
-                for content in audio_generator
-            )
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
 
-            responses = client.streaming_recognize(streaming_config, requests)
+            num_chars_printed = len(transcript)
+        else:
+            print(transcript + overwrite_chars)
 
-            listen_print_loop(responses)
+            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                print("Exiting...")
+                break
+            num_chars_printed = 0
+
+    return transcript    
+
+def main():
+    language_code = "en-US"
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code,
+    )
     
-    if __name__ == "__main__":
-        main()
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config,
+        interim_results=True,
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (speech.StreamingRecognizeRequest(audio_content=content) for content in audio_generator)
+        responses = client.streaming_recognize(streaming_config, requests)
+        listen_print_loop(responses)
+
+if __name__ == "__main__":
+    main()
